b43957d2b2ce27461b2831008b2543e1fc057505
Abhishek Singh <abhishek.singh@mavq.com>
2021-07-20 08:37:43 +0530

    Total Sale and total purchase price

--- FrontEnd/src/app/components/orders/orders.component.ts
+++ FrontEnd/src/app/components/orders/orders.component.ts
@@ -11,18 +11,34 @@ import {OrderService} from '../../order/order.service';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  soldTotal:number;
+  purchaseTotal:number;
 
   constructor(private router: Router,
     private flashMessagesService: FlashMessagesService,
     public orderService: OrderService) { }
 
   ngOnInit(): void {
+    this.soldTotal = 0;
+    this.purchaseTotal = 0;
     if(localStorage.getItem("token")==null){
       this.router.navigate(['/login']);
       this.flashMessagesService.show("Please login to view Orders",{cssClass: 'alert-danger', timeout: 3000});
     }
     this.orderService.getOrder().subscribe((res)=>{
       this.orderService.orders = res as Order[]; 
+      this.totalSum();
+    })
+  }
+
+  totalSum(){
+    this.orderService.orders.forEach(item=>{
+        if(item.action=="Sold"){
+          this.soldTotal = this.soldTotal+item.price;
+        }
+        else{
+          this.purchaseTotal = this.purchaseTotal+item.price;
+        }
     })
   }
 }
